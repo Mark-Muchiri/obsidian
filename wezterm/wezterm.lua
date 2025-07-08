b3c223ce15e2b27a45c55776079bb0a0124f8be6
Mark-Muchiri <muriithimac@gmail.com>
2025-07-08 13:05:51 +0300

    wez update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,9 +5,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Appearance
-config.initial_cols = 75
-config.initial_rows = 35
-config.font_size = 10.6
+config.initial_cols = 80
+config.initial_rows = 33
+config.font_size = 10.8
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.default_cursor_style = "BlinkingBar"
 config.cursor_blink_rate = 700
