f57fb815f8f8e1ca62583b2d7cb4a4c8cab6853e
Mark-Muchiri <muriithimac@gmail.com>
2025-07-19 22:51:31 +0300

    modified:   wezterm/wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,8 +16,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.pane_focus_follows_mouse = true
 config.front_end = "OpenGL"
 config.term = "wezterm"
-config.color_scheme = "Argonaut"
--- config.color_scheme = "Argonaut (Gogh)"
+config.color_scheme = "Argonaut (Gogh)"
+-- config.color_scheme = "Argonaut"
 -- config.color_scheme = "duckbones"
 -- config.color_scheme = "Aura (Gogh)"
 
@@ -27,52 +27,43 @@ config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = false
 config.tab_max_width = 999
 function get_max_cols(window)
-  local tab = window:active_tab()
-  local cols = tab:get_size().cols
-  return cols
+	local tab = window:active_tab()
+	local cols = tab:get_size().cols
+	return cols
 end
 
-wezterm.on(
-  'window-config-reloaded',
-  function(window)
-    wezterm.GLOBAL.cols = get_max_cols(window)
-  end
-)
+wezterm.on("window-config-reloaded", function(window)
+	wezterm.GLOBAL.cols = get_max_cols(window)
+end)
 
-wezterm.on(
-  'window-resized',
-  function(window, pane)
-    wezterm.GLOBAL.cols = get_max_cols(window)
-  end
-)
+wezterm.on("window-resized", function(window, pane)
+	wezterm.GLOBAL.cols = get_max_cols(window)
+end)
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local title = tab.active_pane.title
-    local full_title = '[' .. tab.tab_index + 1 .. '] ' .. title
-    local pad_length = (wezterm.GLOBAL.cols // #tabs - #full_title) // 2
-    if pad_length * 2 + #full_title > max_width then
-      pad_length = (max_width - #full_title) // 2
-    end
-    return string.rep(' ', pad_length) .. full_title .. string.rep(' ', pad_length)
-  end
-)
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab.active_pane.title
+	local full_title = "[" .. tab.tab_index + 1 .. "] " .. title
+	local pad_length = (wezterm.GLOBAL.cols // #tabs - #full_title) // 2
+	if pad_length * 2 + #full_title > max_width then
+		pad_length = (max_width - #full_title) // 2
+	end
+	return string.rep(" ", pad_length) .. full_title .. string.rep(" ", pad_length)
+end)
 
 -- Keybindings
 config.keys = {
-  -- vertical split
-  {
-    key = 'o',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  -- horizontal split
-  {
-    key = 'i',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
+	-- vertical split
+	{
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- horizontal split
+	{
+		key = "i",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 }
 
 -- Finally, return the configuration to wezterm:
