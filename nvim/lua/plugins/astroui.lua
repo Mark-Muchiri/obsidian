d6f556105ad73e3bd4b79b62adfc8ed7af61dca8
Mark-Muchiri <muriithimac@gmail.com>
2025-07-22 02:17:44 +0300

    tinkering

--- nvim/lua/plugins/astroui.lua
+++ nvim/lua/plugins/astroui.lua
@@ -5,13 +5,34 @@
 
 ---@type LazySpec
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("onedark").setup {
-      style = "deep",
-    }
-    -- Enable theme
-    require("onedark").load()
-  end,
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    -- colorscheme = "onedark",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    highlights = {
+      init = { -- this table overrides highlights in all themes
+        -- Normal = { bg = "NONE" },
+      },
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+        -- Normal = { bg = "#000000" },
+      },
+    },
+
+    -- Icons can be configured throughout the interface
+    icons = {
+      -- configure the loading of the lsp in the status line
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
+  },
 }
---
+++ nvim/lua/plugins/onedark.lua
@@ -0,0 +1,33 @@
+return {
+  vim.defer_fn(function()
+    -- ... existing configuration ...
+
+    -- Use terminal's background color instead of "NONE"
+    local transparent_bg = vim.fn.has "gui_running" == 1 and "NONE" or ""
+
+    -- Enhanced Tabline Fixes
+    vim.api.nvim_set_hl(0, "BufferLineFill", { bg = transparent_bg }) -- Background of the entire tabline
+    vim.api.nvim_set_hl(0, "BufferLineBackground", { bg = transparent_bg }) -- Background of inactive tabs
+
+    -- Fix typos in group names:
+    vim.api.nvim_set_hl(0, "BufferLineSeparator", { bg = transparent_bg, fg = "#5c6370" })
+    vim.api.nvim_set_hl(0, "BufferLineTabSelected", { bg = transparent_bg })
+    vim.api.nvim_set_hl(0, "BufferLineTabClose", { bg = transparent_bg })
+
+    -- Additional groups for full transparency
+    vim.api.nvim_set_hl(0, "BufferLineTab", { bg = transparent_bg })
+    vim.api.nvim_set_hl(0, "BufferLineOffsetSeparator", { bg = transparent_bg })
+
+    -- ... rest of your configuration ...
+  end, 20000),
+
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("onedark").setup {
+      style = "deep",
+    }
+    -- Enable theme
+    require("onedark").load()
+  end,
+}
